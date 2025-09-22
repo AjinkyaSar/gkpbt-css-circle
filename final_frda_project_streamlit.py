43f07596ca14b2a4fcc701fef558b8bdd87d806a
AjinkyaSar <40080037+AjinkyaSar@users.noreply.github.com>
2025-09-22 08:33:34 +0530

    Update final_frda_project_streamlit.py

--- final_frda_project_streamlit.py
+++ final_frda_project_streamlit.py
@@ -9,13 +9,11 @@ Original file is located at
 
 import numpy as np
 import pandas as pd
-import statsmodels.api as sm
 
 import streamlit as st
 import pandas as pd
 import numpy as np
 import seaborn as sns
-import matplotlib.pyplot as plt
 
 # --- Page management ---
 if "page" not in st.session_state:
